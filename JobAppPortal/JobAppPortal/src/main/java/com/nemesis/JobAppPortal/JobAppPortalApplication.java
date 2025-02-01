c418fbcf3db7b86746f25a35bafab1a724694ee4
Yogananda-09 <105623703+Yogananda-09@users.noreply.github.com>
2025-02-01 20:10:35 +0530

    Add files via upload

---
+++ JobAppPortal/JobAppPortal/src/main/java/com/nemesis/JobAppPortal/JobAppPortalApplication.java
@@ -0,0 +1,13 @@
+package com.nemesis.JobAppPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobAppPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobAppPortalApplication.class, args);
+	}
+
+}
---
+++ JobAppPortal/JobAppPortal/src/test/java/com/nemesis/JobAppPortal/JobAppPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nemesis.JobAppPortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobAppPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
